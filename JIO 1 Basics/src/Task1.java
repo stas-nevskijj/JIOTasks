74141c3888b2a738105e5c18bea6e23c05de483e
stas-nevskijj <102740749+stas-nevskijj@users.noreply.github.com>
2022-04-03 13:54:23 +0300

    Test

---
+++ JIO 1 Basics/src/Task1.java
@@ -0,0 +1,17 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		double a, b, c;
+		a = 3;
+		b = 3;
+		c = -2;
+		System.out.println(z(a, b, c));
+
+	}
+	
+	public static double z(double a, double b, double c) {
+		return ((a - 3) * b / 2) + c;
+	}
+
+}
